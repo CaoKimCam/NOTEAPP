15b0c15b413e91c9f48f150a2fe05a3cf86c939b
CaoKimCam <121728599+CaoKimCam@users.noreply.github.com>
2023-11-11 12:35:56 +0700

    Add files via upload

---
+++ DOANPHATTRIEN/app/src/main/java/com/example/sqlite/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.sqlite;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import com.example.sqlite.Database.DatabaseHelper;
+
+public class MyApplication {
+    Context context;
+}
